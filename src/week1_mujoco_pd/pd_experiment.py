f2a898d4a8802a611387c5ca24f0801465220d4e
saul <2678105502@qq.com>
2026-08-07 04:46:23 +0800

    implement 7 dof pd controller

--- src/week1_mujoco_pd/pd_experiment.py
+++ src/week1_mujoco_pd/pd_experiment.py
@@ -3,20 +3,21 @@ import mujoco.viewer
 import time
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 class PDController():
     """
     PD控制器
     """
     def __init__(self,target,kp,kd):
-        self.target = target
-        self.kp = kp
-        self.kd = kd
+        self.target = np.array(target)
+        self.kp = np.array(kp)
+        self.kd = np.array(kd)
     
     def compute(self,q,vel):
         error = self.target - q
         ctrl = (self.kp * error - self.kd * vel)
-        return ctrl
+        return ctrl,error
     
 class DataLogger:
     """
@@ -31,10 +32,10 @@ class DataLogger:
 
     def record(self,time,q,target,error,ctrl):
         self.time.append(time)
-        self.q.append(q)
-        self.target.append(target)
-        self.error.append(error)
-        self.ctrl.append(ctrl)
+        self.q.append(q.copy())
+        self.target.append(target.copy())
+        self.error.append(error.copy())
+        self.ctrl.append(ctrl.copy())
 
 class MujocoSimulator:
     """
@@ -52,12 +53,12 @@ class MujocoSimulator:
         mujoco.mj_forward(self.model,self.data)
     
     def step(self):
-        q = self.data.qpos[0]
-        qvel = self.data.qvel[0]
-        ctrl = self.controller.compute(q,qvel)
-        self.data.ctrl[0] = ctrl
+        q = self.data.qpos[:7].copy()
+        qvel = self.data.qvel[:7].copy()
+        ctrl,error = self.controller.compute(q,qvel)
+        self.data.ctrl[:7] = ctrl
         mujoco.mj_step(self.model,self.data)
-        self.logger.record(self.data.time,q,self.controller.target,self.controller.target-q,ctrl)
+        self.logger.record(self.data.time,q,self.controller.target,error,ctrl)
     
     def run_with_viewer(self,sim_time):
         with mujoco.viewer.launch_passive(self.model,self.data) as viewer:
@@ -78,32 +79,36 @@ class MujocoSimulator:
         else:
             self.run_without_viewer(sim_time)
 
-class Plot:
+class Plotter:
     def __init__(self,logger):
         self.logger = logger
 
     def plot(self):
-        plt.figure(figsize=(8,6))
-        plt.plot(self.logger.time,self.logger.q,label="q_pos")
-        plt.plot(self.logger.time,self.logger.target,"--")
-        plt.xlabel("time(s)")
-        plt.ylabel("q_pos(rad)")
-        plt.legend()
-        plt.grid()
+        plt.figure(figsize=(10,8))
+        q = np.array(self.logger.q)
+        target = np.array(self.logger.target)
+        for i in range(7):
+            plt.subplot(4,2,i+1)
+            plt.plot(self.logger.time,q[:,i],label="q_pos")
+            plt.plot(self.logger.time,target[:,i],"--")
+            plt.xlabel("time(s)")
+            plt.ylabel("q_pos(rad)")
+            plt.legend()
+            plt.grid()
         plt.show()
 
 if __name__ == "__main__":
-    
+
     current_dir = os.path.dirname(os.path.abspath(__file__))
     xml_path = os.path.join(current_dir,"../../models/franka_panda/franka_emika_panda/panda.xml")
 
-    target = 2.0
-    kp = 50
-    kd = 5
+    target = np.array([0.5,-0.3,0.2,-1.2,0.3,1.0,0.0])
+    kp = np.array([50,50,50,50,50,50,50])
+    kd = np.array([5,5,5,5,5,5,5])
     sim_time = 2.0
     controller = PDController(target=target,kp=kp,kd=kd)
     logger = DataLogger()
-    plot = Plot(logger=logger)
+    plot = Plotter(logger=logger)
     simulator = MujocoSimulator(xml_path=xml_path,controller=controller,logger=logger,render=True)
 
     simulator.reset()
